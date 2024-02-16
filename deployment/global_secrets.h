2a3fde58e34da96a17725e8eec3e9bd70a2e58cd
Chandler Scott <chandler.scott.dev@gmail.com>
2024-02-16 14:12:58 -0500

    cleanup

--- deployment/global_secrets.h
+++
@@ -1,5 +0,0 @@
-#define VALIDATION_KEY (uint8_t[]){2, 4, 8, 2, 5, 4, 0, 1, 1, 2, 1, 6, 5, 8, 3, 8, 3, 4, 6, 6, 5, 0, 1, 8, 7, 6, 3, 5, 4, 3, 2, 6}
-#define SECURE_MESSAGING_KEY (uint8_t[]){9, 3, 6, 1, 1, 1, 4, 8, 8, 9, 5, 2, 5, 2, 6, 7, 5, 4, 8, 9, 6, 2, 5, 1, 7, 8, 2, 6, 0, 9, 3, 1}
-#define VALIDATION_COUNTER 71
-#define SECURE_MESSAGING_COUNTER 624
-#define FLASH_MAGIC 0xD6B21FA1
