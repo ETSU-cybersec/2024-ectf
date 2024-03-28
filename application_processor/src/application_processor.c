df08482369f03fdebb096dc9c90ec8646bc7d9d0
Chandler Scott <chandler.scott.dev@gmail.com>
2024-03-27 22:05:44 -0400

    refactored list cmd

--- application_processor/src/application_processor.c
+++ application_processor/src/application_processor.c
@@ -279,13 +279,13 @@ int scan_components() {
         // Send out command and receive result
         //int len = issue_cmd(addr, transmit_buffer, receive_buffer);
 
-        int result = send_packet(addr, sizeof(uint8_t), transmit);
+        int result = send_packet(addr, sizeof(uint8_t), transmit_buffer);
         if (result == ERROR_RETURN) {
             return ERROR_RETURN;
         }
         
         // Receive message
-        int len = poll_and_receive_packet(addr, receive);
+        int len = poll_and_receive_packet(addr, receive_buffer);
         if (len == ERROR_RETURN) {
             return ERROR_RETURN;
         }
